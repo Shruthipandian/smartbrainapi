33bcef200610c73b6dafac4f4a9119fde1f54ba6
shruthipandian <shruthi.pandian99@gmail.com>
2021-06-28 21:13:15 +0530

    adding port

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.post('/imageurl',(req,res)=>{image.handleapicall(req,res)})
 
 
 
-app.listen(3000,()=>{
-    console.log('app is run on port 3000')
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('app is run on port ${process.env.PORT}')
 })
